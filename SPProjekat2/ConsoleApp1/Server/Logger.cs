8ff888ec97e486b1bbc3d81608eab0f02a5edf12
Miljan <miljan.dragovic@elfak.rs>
2025-09-29 21:07:27 +0200

    Dodato vreme za logger

--- SPProjekat2/ConsoleApp1/Server/Logger.cs
+++ SPProjekat2/ConsoleApp1/Server/Logger.cs
@@ -16,15 +16,15 @@ namespace SPProjekat2.Server
 
         public static void Info(string tag, string message)
         {
-            Console.WriteLine(tag + " [INFO] " + message);
+            Console.WriteLine(DateTime.Now + "\t" + tag + " [INFO] " + message);
         }
         public static void Warning(string tag, string message)
         {
-            Console.WriteLine(tag + " [WARNING] " + message);
+            Console.WriteLine(DateTime.Now + "\t" + tag + " [WARNING] " + message);
         }
         public static void Error(string tag, string message)
         {
-            Console.Error.WriteLine(tag + " [ERROR] " + message);
+            Console.Error.WriteLine(DateTime.Now + "\t" + tag + " [ERROR] " + message);
         }
     }
 }
